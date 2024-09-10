4c9b0e032dced1243b9aba898663d33bfc034a27
HarryLi2028 <Hli2@loyola.edu>
2024-09-10 17:06:35 -0400

    for partner to write my information

--- Lab0.py
+++ Lab0.py
@@ -1,3 +1,13 @@
+# Programmers:  [Harry Li & Andrew Leinbach]
+# Course:  CS151, Dr. Zee
+# Due Date: 9/12/24
+# Lab Assignment: 1
+# Problem Statement:  Output information about the programmers
+# Data In: None, there is no user input in this program
+# Data Out:  The names of the programmers and five statements about each of them
+# Credits: [Is your code based on an example in the book, in class, or something else?
+# Reminder: you should never take code from the Internet or another person.]
+
 print ("CS151 Lab 0") #The code outputs "CS151 Lab 0"
 
 print("My partner is Andrew Leinbach.") #code prints the name of my partner
