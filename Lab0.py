5effdcbb8cbe255eadb036b9b5b6b0e5a440ae09
HarryLi2028 <Hli2@loyola.edu>
2024-09-10 16:56:31 -0400

    for partner to write my information

---
+++ Lab0.py
@@ -0,0 +1,10 @@
+print ("CS151 Lab 0") #The code outputs "CS151 Lab 0"
+
+print("My partner is Andrew Leinbach.") #code prints the name of my partner
+
+#codes outputs 5 different facts about my partner
+print("He is from Ellicott City, Maryland.")
+print("He is a data Science major.")
+print("He lives in Hammerman hall.")
+print("His dad and brother are both Aluminis.")
+print("His favorite sports are golf and basketball.")
